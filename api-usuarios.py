08adb7a5ea0510407145c42f743984bafc6a772d
GoguX76 <se.godoyr@duocuc.cl>
2025-11-11 18:01:36 -0300

    Implementación de funciones que llaman a las apis para utilizarlas en el main. Nueva ruta api y correcciones de sintaxis. Código aún no completo.

--- api-usuarios.py
+++ api-usuarios.py
@@ -44,7 +44,7 @@ def obtener_usuario_id(id: int):
             }
     
     #El HTTPException es para lanzar un error en caso de que el codigo recibido sea x con un mensaje detallado.
-    return HTTPException(
+    raise HTTPException(
         status_code=404,
         detail=f"El usuario con id {id} no ha sido encontrado en el sistema"
     )
@@ -76,3 +76,33 @@ def registrar_usuarios(usuario: Usuario):
     return {
         "mensaje": "¡El usuario ha sido registrado de manera exitosa!"
     }
+
+@app.put("usuarios/actualizar/{id}")
+def actualizar_usuario(usuario: Usuario, id: int):
+    for i, usuario in enumerate(usuarios_sistema,1):
+        if usuario['id'] == id:
+            if not validacionUsuario(usuario.Nombre):
+                raise HTTPException(status_code=400, detail="Nombre inválido")
+            
+            if not validacionEdad(usuario.Edad):
+                raise HTTPException(status_code=400, detail="Edad inválida")
+            
+            if not validacionGenero(usuario.Genero):
+                raise HTTPException(status_code=400, detail="Genero inválido")
+
+            genero_completo = "Masculino" if usuario.Genero == "M" else "Femenino"
+
+            usuarios_sistema[i] = {
+                "id": id,
+                "Nombre": usuario.Nombre,
+                "Edad": usuario.Edad,
+                "Genero": genero_completo
+            }
+
+            return {
+                "mensaje": "Los datos del usuario han sido actualizados correctamente"
+            }
+    raise HTTPException(
+        status_code = 404,
+        detail = "El usuario no ha sido encontrado en el sistema"
+    )
--- functions.py
+++ functions.py
@@ -1,4 +1,76 @@
+import requests as rq
 import random as rd
+
+API_URL = "http://localhost:8000"
+
+def crear_usuario_api():
+    print("Para empezar, ingrese su nombre de usuario")
+    nom = input(">> ")
+
+    print("Ahora, ingrese su edad")
+    age = input(">> ")
+
+    print("Por último, indique su genero")
+    gender = input(">> ")
+
+    data = {
+        "Nombre": nom,
+        "Edad": age,
+        "Genero": gender
+    }
+
+    response = rq.post(f"{API_URL}/usuarios/registrar", json=data)
+
+    if response.status_code == 200:
+        print("El usuario ha sido registrado con éxito")
+    else:
+        print(f"Error: {response.json()}")
+
+def mostrar_usuarios_api():
+    response = rq.get(f"{API_URL}/usuarios")
+
+    if response.status_code == 200:
+        data = response.json()
+        print(f"=== TOTAL USUARIOS: {len(data)} ===\n")
+        
+        for i, usuario in enumerate(data, 1):
+            print(f"ID: {usuario['id']}")
+            print(f"Nombre: {usuario['Nombre']}")
+            print(f"Edad: {usuario['Edad']}")
+            print(f"Genero: {usuario['Genero']}")
+            print("-" * 40)
+
+    else:
+        print(f"Error al obtener a los usuarios")
+
+def buscar_usuario_id():
+    print("Ingrese el ID del usuario del que desea saber sus datos")
+    user_id = int(input(">> "))
+
+    response = rq.get(f"{API_URL}/usuarios/{user_id}")
+
+    if response.status_code == 200:
+        user = response.json()
+        for i, usuario in enumerate(user, 1):
+            print(f"ID: {usuario['id']}")
+            print(f"Nombre: {usuario['Nombre']}")
+            print(f"Edad: {usuario['Edad']}")
+            print(f"Genero: {usuario['Genero']}")
+            print("-" * 40)
+
+    else:
+        print("El usuario no ha sido encontrado")
+
+def actualizar_usuario_api():
+    print("Ingrese el ID del usuario del que desea actualizar sus datos")
+    user_id = int(input(">> "))
+
+    response_check = rq.get(f"")
+
+    response = rq.put(f"{API_URL}/usuarios/actualizar/{user_id}")
+
+    
+
 
 #Función que valida que el usuario haya ingresado bien el nombre.
 def validacionUsuario(nom):
